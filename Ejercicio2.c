a8cad8fcdb52abbb4e6e7c9dbc6c8d652961f69b
farlight75 <farithazuara@gmail.com>
2025-10-17 13:44:17 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+int main()
+{
+  int num1, num2;
+
+  printf("Ingrese el primer número entero: \n");
+  scanf("%d", &num1);
+
+  printf("Ingrese el segundo número entero: \n");
+  scanf("%d", &num2);
+
+  if (num2 == 0) 
+{
+  printf("No se puede dividir entre cero.\n");
+} 
+else
+} 
+  printf("El primer número NO es divisible entre el segundo.\n");
+}
+   
+}
